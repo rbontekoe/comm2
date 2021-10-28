1ad54c4681c45b91ec50200cd50ca46353acb19b
Rob Bontekoe <rbontekoe@appligate.nl>
2021-10-28 14:18:37 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,36 @@
+function toonGegevens (receivedData: string) {
+    array = receivedData.split("|")
+    i = 0
+    for (let index = 0; index < array.length; index++) {
+        basic.showString("" + (array[i]))
+        i += 1
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    data = "20|1030|40"
+    array = data.split("|")
+    i = 0
+    for (let index = 0; index < array.length; index++) {
+        basic.showString("" + (array[i]))
+        i += 1
+    }
+})
+radio.onReceivedString(function (receivedString) {
+    data = receivedString
+    toonGegevens(data)
+})
+input.onButtonPressed(Button.B, function () {
+    data = "20|1030|40"
+    toonGegevens(data)
+})
+let data = ""
+let i = 0
+let array: string[] = []
+radio.setGroup(10)
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # . # . #
+    # . . . #
+    . # # # .
+    `)
